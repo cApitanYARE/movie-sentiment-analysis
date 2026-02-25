652059f21c1cb14ac8664f35696cb124b9b98181
cApitanYARE <deniswatch1@gmail.com>
2026-02-25 20:27:51 +0200

    w/o tensorflow

--- fastAPI/api.py
+++ fastAPI/api.py
@@ -1,7 +1,7 @@
 
 from fastapi import FastAPI
 from pydantic import BaseModel, Field, field_validator
-import tensorflow as tf
+import tflite_runtime.interpreter as tflite
 import joblib
 import uvicorn
 
@@ -11,6 +11,8 @@ import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 import re
+
+import json
 
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -24,13 +26,18 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+interpreter = tflite.Interpreter(model_path="Model/sentiment_model.tflite")
+interpreter.allocate_tensors()
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
 
-model = tf.keras.models.load_model('Model/sentiment_model.keras')
-tokenizer = joblib.load("Model/tokenizer.joblib")
+with open("Model/tokenizer_index.json", "r", encoding="utf-8") as f:
+    word_index = json.load(f)
 
-nltk.download('punkt')
-nltk.download('punkt_tab')
-nltk.download('stopwords')
+def texts_to_sequences_manual(text, word_index):
+    words = text.split()
+    sequence = [word_index.get(w, 0) for w in words]
+    return [sequence]
 
 stop_words = set(stopwords.words('english'))
 
@@ -64,14 +71,18 @@ def root():
           description = 'Takes a text and returns the probability of a positive sentiment and the class (‘positive’ or ‘negative’).',
           response_description= 'Sentiment prediction result')
 def predict(data: inputData):
-    clear_text = [clean_text(data.text)]
+    clear_text = clean_text(data.text)
 
-    seq = tokenizer.texts_to_sequences(clear_text)
-    padded = tf.keras.preprocessing.sequence.pad_sequences(
-        seq, maxlen=200, padding='post', truncating='post'
-    )
+    seq = texts_to_sequences_manual(clear_text, word_index)
 
-    prediction = model.predict(padded)
+    max_len = 200
+    padded = np.zeros((32, max_len), dtype=np.float32)
+    for i, val in enumerate(seq[0][:max_len]):
+        padded[0, i] = val
+
+    interpreter.set_tensor(input_details[0]['index'], padded)
+    interpreter.invoke()
+    prediction = interpreter.get_tensor(output_details[0]['index'])
 
     sentiment = "positive" if prediction[0][0] > 0.5 else "negative"
 
